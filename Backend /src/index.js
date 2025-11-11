b10596310cc91c42ebff7404dd61ef203e23b78f
saurabh kamane <kamanesaurabh68@gmail.com>
2025-11-11 21:00:42 +0530

    first commit

---
+++ Backend /src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv"
+import {app} from "./app.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:"./.env"
+})
+
+const PORT=process.env.PORT || 9000
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`server is running on the port http://localhost:${PORT}`)
+})
+}).
+catch((err)=>{
+    console.log("Mongodb connection error = ",err)
+})
+
