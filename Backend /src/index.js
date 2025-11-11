75ce50bdb817aec3e692c70d68e6266c248f411d
saurabh kamane <kamanesaurabh68@gmail.com>
2025-11-11 21:32:24 +0530

    error ads

--- Backend /src/index.js
+++ Backend /src/index.js
@@ -1,12 +1,16 @@
 import dotenv from "dotenv"
 import {app} from "./app.js"
 import connectDB from "./db/index.js"
-
 dotenv.config({
     path:"./.env"
 })
-
 const PORT=process.env.PORT || 9000
+
+
+
+
+
+
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
@@ -16,4 +20,3 @@ connectDB().then(()=>{
 catch((err)=>{
     console.log("Mongodb connection error = ",err)
 })
-
